a8454be3a1dc4b2931f7f7b96b3b880f45e852b0
Bumble <nick1092387456@gmail.com>
2022-07-01 14:41:17 +0800

    setup: tutorial sequelize model

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.use(cors(corsOptions))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+const db = require('./app/models')
+db.sequelize.sync({ force: true }).then(() => {
+  console.log('Drop and re-sync db.')
+})
+
 //simple route
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to my application.' })
